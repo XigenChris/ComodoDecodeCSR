facf1043fc0a5ca40e9f2e86f13fda0a60f5961b
Chris Hilsdon <chrish@xigen.co.uk>
2016-04-15 13:47:32 +0100

    Added missing namespace for guzzle

--- src/ComodoDecodeCSR.php
+++ src/ComodoDecodeCSR.php
@@ -10,6 +10,7 @@
 namespace Xigen;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class ComodoDecodeCSR
 {
@@ -63,7 +64,7 @@ class ComodoDecodeCSR
         } catch (ClientException $e) {
             return false;
         }
-        
+
         $responce = "" . $request->getBody();
         return ($responce === $this->generateDVC());
     }
